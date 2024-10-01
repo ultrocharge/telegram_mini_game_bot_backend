b7ac3db45e05ebf3f4065b15727460f19cd689ee
ultrocharge <ultrocharge@gmail.com>
2024-10-01 12:02:02 -0700

    refactor: get test console

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use('/moverz', require('./routes/bot'))
-
+app.get("/", () => {
+    console.log("Server is running correctly!")
+})
 moverzbot()
 
 app.listen(5000, console.log("Server running on port 5000"))
